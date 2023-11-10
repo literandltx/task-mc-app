57db0f5ce851789cbd505a986de9f429105e9eab
literandltx <romanskovronn@gmail.com>
2023-11-10 17:56:09 +0200

    infra/add-basic-controllers

---
+++ src/main/java/com/literandltx/taskmcapp/controller/AttachmentController.java
@@ -0,0 +1,10 @@
+package com.literandltx.taskmcapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/attachments")
+@RestController
+public class AttachmentController {
+
+}
---
+++ src/main/java/com/literandltx/taskmcapp/controller/LabelController.java
@@ -0,0 +1,10 @@
+package com.literandltx.taskmcapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/labels")
+@RestController
+public class LabelController {
+
+}
