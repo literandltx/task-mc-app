dc7ecce10f8e3ecc98c9b8ecb8a5dc8c7ad9d1d1
literandltx <romanskovronn@gmail.com>
2023-11-11 00:08:16 +0200

    infra/add ProjectController methods

--- src/main/java/com/literandltx/taskmcapp/controller/ProjectController.java
+++ src/main/java/com/literandltx/taskmcapp/controller/ProjectController.java
@@ -1,10 +1,38 @@
 package com.literandltx.taskmcapp.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/projects")
 @RestController
 public class ProjectController {
+    @PostMapping
+    public void createNewProject() {
 
+    }
+
+    @GetMapping
+    public void retrieveUserProjects() {
+
+    }
+
+    @GetMapping
+    public void retrieveProjectDetails(@PathVariable Long id) {
+
+    }
+
+    @PutMapping
+    public void updateProject(@PathVariable Long id) {
+
+    }
+
+    @DeleteMapping
+    public void deleteProject(@PathVariable Long id) {
+
+    }
 }
