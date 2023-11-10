d81009aabda7cbb2949c5fc042d01b345f3a9846
literandltx <romanskovronn@gmail.com>
2023-11-10 15:42:30 +0200

    init

---
+++ src/main/java/com/literandltx/taskmcapp/TaskMCAppApplication.java
@@ -0,0 +1,13 @@
+package com.literandltx.taskmcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMCAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMCAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/literandltx/taskmcapp/TaskMCAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literandltx.taskmcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMCAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
