c69d3ed83aef74bef5f8ed3d06a7ab10920fc22a
literandltx <romanskovronn@gmail.com>
2023-11-10 16:33:10 +0200

    fix/rename main boot class

--- src/main/java/com/literandltx/taskmcapp/TaskMcAppApplication.java
+++ src/main/java/com/literandltx/taskmcapp/TaskMcAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskMCAppApplication {
+public class TaskMcAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TaskMCAppApplication.class, args);
+        SpringApplication.run(TaskMcAppApplication.class, args);
     }
 
 }
