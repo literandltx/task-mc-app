15dcd4487a9ecabfc5d54403cafc6ee5b65ae854
literandltx <romanskovronn@gmail.com>
2023-11-11 00:02:39 +0200

    infra/add AuthenticationController methods

--- src/main/java/com/literandltx/taskmcapp/controller/AuthenticationController.java
+++ src/main/java/com/literandltx/taskmcapp/controller/AuthenticationController.java
@@ -1,10 +1,19 @@
 package com.literandltx.taskmcapp.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/auth")
 @RestController
 public class AuthenticationController {
+    @PostMapping("/login")
+    public void login() {
 
+    }
+
+    @PostMapping("/register")
+    public void register() {
+
+    }
 }
