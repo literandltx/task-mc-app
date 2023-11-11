f36dce0b88f404987365b045afc5835c09f3221f
literandltx <romanskovronn@gmail.com>
2023-11-11 16:48:32 +0200

    infra/add UsersController methods

--- src/main/java/com/literandltx/taskmcapp/controller/UsersController.java
+++ src/main/java/com/literandltx/taskmcapp/controller/UsersController.java
@@ -1,10 +1,26 @@
 package com.literandltx.taskmcapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/users")
 @RestController
 public class UsersController {
+    @PutMapping("/{id}/role")
+    public void updateUserRole(@PathVariable Long id) {
 
+    }
+
+    @GetMapping("/me")
+    public void getMyProfileInfo() {
+
+    }
+
+    @PutMapping("/me")
+    public void updateProfileInfo() {
+
+    }
 }
