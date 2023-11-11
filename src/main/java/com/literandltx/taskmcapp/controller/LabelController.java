bc3322e03a7ae4f499db6366c9a3484ea7f258f1
literandltx <romanskovronn@gmail.com>
2023-11-11 16:45:09 +0200

    infra/add LabelController methods

--- src/main/java/com/literandltx/taskmcapp/controller/LabelController.java
+++ src/main/java/com/literandltx/taskmcapp/controller/LabelController.java
@@ -1,10 +1,32 @@
 package com.literandltx.taskmcapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/labels")
 @RestController
 public class LabelController {
+    @PostMapping
+    public void createNewLabel() {
 
+    }
+
+    @GetMapping
+    public void retrieveLabels() {
+
+    }
+
+    @PutMapping("/{id}")
+    public void updateLabel(@PathVariable Long id) {
+
+    }
+
+    @PutMapping("/{id}")
+    public void deleteLabel(@PathVariable Long id) {
+
+    }
 }
