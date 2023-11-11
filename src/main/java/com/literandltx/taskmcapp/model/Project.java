52fba7a1b21a8f28b6c5c9e069be663045e024a4
literandltx <romanskovronn@gmail.com>
2023-11-11 18:16:41 +0200

    boot/config liquibase and add Project mock model

---
+++ src/main/java/com/literandltx/taskmcapp/model/Project.java
@@ -0,0 +1,17 @@
+package com.literandltx.taskmcapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
