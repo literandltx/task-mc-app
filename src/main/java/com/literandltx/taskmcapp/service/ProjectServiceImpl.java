e7493513b1c8fc26977c2d25a8cc1ef714564f3c
literandltx <romanskovronn@gmail.com>
2023-11-12 18:37:56 +0200

    app/add impl ProjectController method with service and repository

---
+++ src/main/java/com/literandltx/taskmcapp/service/ProjectServiceImpl.java
@@ -0,0 +1,54 @@
+package com.literandltx.taskmcapp.service;
+
+import com.literandltx.taskmcapp.dto.project.ProjectRequestDto;
+import com.literandltx.taskmcapp.dto.project.ProjectRespondDto;
+import com.literandltx.taskmcapp.mapper.ProjectMapper;
+import com.literandltx.taskmcapp.model.Project;
+import com.literandltx.taskmcapp.repository.ProjectRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ProjectServiceImpl implements ProjectService {
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+
+    @Override
+    public ProjectRespondDto save(ProjectRequestDto requestDto) {
+        Project model = projectMapper.toModel(requestDto);
+
+        return projectMapper.toDto(projectRepository.save(model));
+    }
+
+    @Override
+    public List<ProjectRespondDto> findAll(Pageable pageable) {
+        return projectRepository.findAll(pageable).stream()
+                .map(projectMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public ProjectRespondDto findById(Long id) {
+        return projectMapper.toDto(projectRepository.findById(id).orElseThrow(RuntimeException::new));
+    }
+
+    @Override
+    public ProjectRespondDto updateById(Long id, ProjectRequestDto requestDto) {
+        if (!projectRepository.existsById(id)) {
+            throw new RuntimeException("Entity not found");
+        }
+        Project model = projectMapper.toModel(requestDto);
+        model.setId(id);
+
+        Project saved = projectRepository.save(model);
+        return projectMapper.toDto(saved);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        projectRepository.deleteById(id);
+    }
+}
