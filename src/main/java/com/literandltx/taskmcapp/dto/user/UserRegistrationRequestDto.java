6284cda76b8b8a4a09fb119f4cca594aafffdab4
literandltx <romanskovronn@gmail.com>
2023-11-14 23:05:39 +0200

    app/update user registration validation

--- src/main/java/com/literandltx/taskmcapp/dto/user/UserRegistrationRequestDto.java
+++ src/main/java/com/literandltx/taskmcapp/dto/user/UserRegistrationRequestDto.java
@@ -1,13 +1,41 @@
 package com.literandltx.taskmcapp.dto.user;
 
+import com.literandltx.taskmcapp.validation.FieldMatch;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@FieldMatch(
+        first = "password",
+        second = "repeatPassword",
+        message = "Password and repeat password shouldn't be empty and should be equal"
+)
 public class UserRegistrationRequestDto {
+    @Size.List(value = {
+            @Size(min = 3, message = "Username length need to be more than 3"),
+            @Size(max = 100, message = "Username length need to be less than 100")
+    })
     private String username;
+
+    @Email
     private String email;
+
+    @Size(min = 8, max = 256)
     private String password;
+
+    @Size(min = 8, max = 256)
     private String repeatPassword;
+
+    @Size.List(value = {
+            @Size(min = 2, message = "Username length need to be more than 2"),
+            @Size(max = 50, message = "Username length need to be less than 50")
+    })
     private String firstName;
+
+    @Size.List(value = {
+            @Size(min = 2, message = "Username length need to be more than 2"),
+            @Size(max = 50, message = "Username length need to be less than 50")
+    })
     private String lastName;
 }
