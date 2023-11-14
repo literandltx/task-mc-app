5a3b3547149dfa351f7900ffdd0f4f626a350e91
literandltx <romanskovronn@gmail.com>
2023-11-14 17:48:35 +0200

    app/update user model, add role model, insert default roles

---
+++ src/main/java/com/literandltx/taskmcapp/model/Role.java
@@ -0,0 +1,42 @@
+package com.literandltx.taskmcapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE roles SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = false")
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name", nullable = false, unique = true)
+    private RoleName name;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+
+    public enum RoleName implements GrantedAuthority {
+        ROLE_ADMIN,
+        ROLE_USER;
+
+        @Override
+        public String getAuthority() {
+            return name();
+        }
+    }
+}
