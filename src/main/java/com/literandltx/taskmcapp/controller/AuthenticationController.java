9cb8c1d1486642c22f5f1839c7c365a1a1a36f44
literandltx <romanskovronn@gmail.com>
2023-11-14 18:03:12 +0200

    app/update authentication controller

--- src/main/java/com/literandltx/taskmcapp/controller/AuthenticationController.java
+++ src/main/java/com/literandltx/taskmcapp/controller/AuthenticationController.java
@@ -1,19 +1,34 @@
 package com.literandltx.taskmcapp.controller;
 
+import com.literandltx.taskmcapp.dto.user.UserLoginRequestDto;
+import com.literandltx.taskmcapp.dto.user.UserLoginResponseDto;
+import com.literandltx.taskmcapp.dto.user.UserRegistrationRequestDto;
+import com.literandltx.taskmcapp.dto.user.UserRegistrationResponseDto;
+import com.literandltx.taskmcapp.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RequestMapping("/auth")
 @RestController
 public class AuthenticationController {
-    @PostMapping("/login")
-    public void login() {
+    private final UserService userService;
 
+    @PostMapping("/login")
+    public UserLoginResponseDto login(
+            @RequestBody @Valid UserLoginRequestDto requestDto
+    ) {
+        throw new UnsupportedOperationException();
     }
 
     @PostMapping("/register")
-    public void register() {
-
+    public UserRegistrationResponseDto register(
+            @RequestBody @Valid UserRegistrationRequestDto requestDto
+    ) {
+        return userService.register(requestDto);
     }
 }
