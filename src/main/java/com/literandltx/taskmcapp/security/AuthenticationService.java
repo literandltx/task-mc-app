2ec83b804f499b54ba19dae6a59d60fe8d74024d
literandltx <romanskovronn@gmail.com>
2023-11-15 16:21:30 +0200

    app/add jwtUtil, AuthenticationService and application.properties

---
+++ src/main/java/com/literandltx/taskmcapp/security/AuthenticationService.java
@@ -0,0 +1,27 @@
+package com.literandltx.taskmcapp.security;
+
+import com.literandltx.taskmcapp.dto.user.UserLoginRequestDto;
+import com.literandltx.taskmcapp.dto.user.UserLoginResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthenticationService {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(requestDto.getUsername(), requestDto.getPassword())
+        );
+
+        String token = jwtUtil.generateToken(requestDto.getUsername());
+        UserLoginResponseDto responseDto = new UserLoginResponseDto();
+        responseDto.setToken(token);
+        return responseDto;
+    }
+}
