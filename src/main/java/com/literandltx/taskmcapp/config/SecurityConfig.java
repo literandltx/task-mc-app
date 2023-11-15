df6b4a79a3488616867bac97d8872abf538e0ac0
literandltx <romanskovronn@gmail.com>
2023-11-15 17:24:56 +0200

    app/add jwt authentication filter

--- src/main/java/com/literandltx/taskmcapp/config/SecurityConfig.java
+++ src/main/java/com/literandltx/taskmcapp/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.literandltx.taskmcapp.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import com.literandltx.taskmcapp.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,11 +15,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -37,6 +40,8 @@ public class SecurityConfig {
                 .httpBasic(withDefaults())
                 .sessionManagement((session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)))
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(userDetailsService)
                 .build();
     }
