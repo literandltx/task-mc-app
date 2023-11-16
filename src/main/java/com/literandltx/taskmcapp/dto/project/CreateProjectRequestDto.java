277632358b752394133713e780754998ca0186f0
literandltx <romanskovronn@gmail.com>
2023-11-16 20:43:29 +0200

    app/update user dto

--- src/main/java/com/literandltx/taskmcapp/dto/project/CreateProjectRequestDto.java
+++ src/main/java/com/literandltx/taskmcapp/dto/project/CreateProjectRequestDto.java
@@ -1,15 +1,14 @@
 package com.literandltx.taskmcapp.dto.project;
 
-import com.literandltx.taskmcapp.model.Project;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import lombok.Data;
 
 @Data
-public class ProjectRespondDto {
-    private Long id;
+public class CreateProjectRequestDto {
+    @NotBlank
     private String name;
     private String description;
-    private Project.Status status;
     private LocalDate startDate;
     private LocalDate endDate;
 }
--- src/main/java/com/literandltx/taskmcapp/dto/project/UpdateProjectRequestDto.java
+++ src/main/java/com/literandltx/taskmcapp/dto/project/UpdateProjectRequestDto.java
@@ -1,14 +1,24 @@
 package com.literandltx.taskmcapp.dto.project;
 
+import com.literandltx.taskmcapp.model.Project;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import lombok.Data;
 
 @Data
-public class ProjectRequestDto {
+public class UpdateProjectRequestDto {
     @NotBlank
     private String name;
+
+    @NotBlank
     private String description;
+
+    @NotBlank
+    private Project.Status status;
+
+    @NotBlank
     private LocalDate startDate;
+
+    @NotBlank
     private LocalDate endDate;
 }
