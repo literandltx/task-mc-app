8e4cef48de8365d0f81b33afbad13c5f0a225df9
literandltx <romanskovronn@gmail.com>
2023-11-28 00:13:39 +0200

    app/add Attachment model

---
+++ src/main/java/com/literandltx/taskmcapp/model/Attachment.java
@@ -0,0 +1,35 @@
+package com.literandltx.taskmcapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "attachments")
+public class Attachment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @Column(name = "filename", nullable = false)
+    private String filename;
+
+    @Column(name = "dropbox_filename", nullable = false)
+    private String dropboxFilename;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
