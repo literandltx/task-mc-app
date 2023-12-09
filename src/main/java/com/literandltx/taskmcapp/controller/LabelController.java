233d3cff55b24ac6e6b649aae3b6e652bd60ffa7
literandltx <romanskovronn@gmail.com>
2023-12-09 15:31:46 +0200

    app/add LabelController api-docs

--- src/main/java/com/literandltx/taskmcapp/controller/LabelController.java
+++ src/main/java/com/literandltx/taskmcapp/controller/LabelController.java
@@ -5,6 +5,8 @@ import com.literandltx.taskmcapp.dto.labels.LabelResponseDto;
 import com.literandltx.taskmcapp.dto.labels.UpdateLabelRequestDto;
 import com.literandltx.taskmcapp.model.User;
 import com.literandltx.taskmcapp.service.app.LabelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +23,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Label manager")
 @RequiredArgsConstructor
 @RequestMapping("/labels")
 @RestController
 public class LabelController {
     private final LabelService labelService;
 
+    @Operation(summary = "Create new label for project")
     @PostMapping
     public LabelResponseDto createNewLabel(
             @RequestBody @Valid CreateLabelRequestDto requestDto,
@@ -37,6 +42,7 @@ public class LabelController {
         return labelService.create(requestDto, projectId, user);
     }
 
+    @Operation(summary = "Retrieve all label in project")
     @GetMapping
     public List<LabelResponseDto> retrieveLabels(
             @RequestParam Long projectId,
@@ -48,9 +54,10 @@ public class LabelController {
         return labelService.findAll(projectId, user, pageable);
     }
 
+    @Operation(summary = "Retrieve label by id in project")
     @GetMapping("/{id}")
-    public LabelResponseDto retrieveLabelsById(
-            Long id,
+    public LabelResponseDto retrieveLabelById(
+            @PathVariable Long id,
             @RequestParam Long projectId,
             Authentication authentication
     ) {
@@ -59,6 +66,7 @@ public class LabelController {
         return labelService.findById(id, projectId, user);
     }
 
+    @Operation(summary = "Update label in project")
     @PutMapping
     public LabelResponseDto updateLabel(
             @RequestBody @Valid UpdateLabelRequestDto requestDto,
@@ -71,6 +79,7 @@ public class LabelController {
         return labelService.updateById(requestDto, id, projectId, user);
     }
 
+    @Operation(summary = "Delete label from project")
     @DeleteMapping
     public void deleteLabel(
             @RequestParam Long id,
@@ -80,10 +89,5 @@ public class LabelController {
         User user = (User) authentication.getPrincipal();
 
         labelService.deleteById(id, projectId, user);
-    }
-
-    @PatchMapping
-    public void assignLabelToTask() {
-        throw new UnsupportedOperationException("Not now");
     }
 }
