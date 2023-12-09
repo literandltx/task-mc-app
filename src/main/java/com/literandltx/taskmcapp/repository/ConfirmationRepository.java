b68beeab205114d47e7125aa793bbf96c264937c
literandltx <romanskovronn@gmail.com>
2023-12-09 22:56:26 +0200

    app/refactor repository-service-controller verifyToken

--- src/main/java/com/literandltx/taskmcapp/repository/ConfirmationRepository.java
+++ src/main/java/com/literandltx/taskmcapp/repository/ConfirmationRepository.java
@@ -1,11 +1,11 @@
 package com.literandltx.taskmcapp.repository;
 
 import com.literandltx.taskmcapp.model.Confirmation;
-import java.util.Optional;
+import com.literandltx.taskmcapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConfirmationRepository extends JpaRepository<Confirmation, Long> {
-    Optional<Confirmation> findByToken(String token);
+    Boolean existsByTokenAndUser(String token, User user);
 }
