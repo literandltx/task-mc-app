44ae433704eec645e507d4179327087a4b9e2284
literandltx <romanskovronn@gmail.com>
2023-12-09 14:43:45 +0200

    app/add AuthenticationController api-docs

--- src/main/java/com/literandltx/taskmcapp/controller/AuthenticationController.java
+++ src/main/java/com/literandltx/taskmcapp/controller/AuthenticationController.java
@@ -7,6 +7,8 @@ import com.literandltx.taskmcapp.dto.user.register.UserRegistrationRequestDto;
 import com.literandltx.taskmcapp.dto.user.register.UserRegistrationResponseDto;
 import com.literandltx.taskmcapp.security.AuthenticationService;
 import com.literandltx.taskmcapp.service.app.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication controller")
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 @RestController
@@ -21,18 +24,20 @@ public class AuthenticationController {
     private final UserService userService;
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "Login by username and password")
     @LogExecutionTime
     @PostMapping("/login")
     public UserLoginResponseDto login(
-            @RequestBody @Valid UserLoginRequestDto requestDto
+            @RequestBody @Valid final UserLoginRequestDto requestDto
     ) {
         return authenticationService.authenticate(requestDto);
     }
 
+    @Operation(summary = "Register method")
     @LogExecutionTime
     @PostMapping("/register")
     public UserRegistrationResponseDto register(
-            @RequestBody @Valid UserRegistrationRequestDto requestDto
+            @RequestBody @Valid final UserRegistrationRequestDto requestDto
     ) {
         return userService.register(requestDto);
     }
