87ebc39dd2cef200112a659ba7cc3ef8277c38ff
literandltx <romanskovronn@gmail.com>
2023-12-17 17:32:59 +0200

    app/add log to auth,email and user service

--- src/main/java/com/literandltx/taskmcapp/aspect/UserAspect.java
+++ src/main/java/com/literandltx/taskmcapp/aspect/UserAspect.java
@@ -1,26 +1,25 @@
 package com.literandltx.taskmcapp.aspect;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Aspect
 @Component
 public class UserAspect {
-    private static final Logger LOGGER = LogManager.getLogger(UserAspect.class);
 
     @Around("@annotation(LogExecutionTime)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-        long start = System.currentTimeMillis();
+        final long start = System.currentTimeMillis();
 
-        Object proceed = joinPoint.proceed();
+        final Object proceed = joinPoint.proceed();
 
-        long executionTime = System.currentTimeMillis() - start;
+        final long executionTime = System.currentTimeMillis() - start;
 
-        LOGGER.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        log.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
 
         return proceed;
     }
