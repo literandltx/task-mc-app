7057e52189c115e4e2bc2cf39bf582f04b671728
literandltx <romanskovronn@gmail.com>
2023-12-19 20:59:45 +0200

    app/add-logging-and-refactor-comment-service

--- src/main/java/com/literandltx/taskmcapp/repository/CommentRepository.java
+++ src/main/java/com/literandltx/taskmcapp/repository/CommentRepository.java
@@ -1,12 +1,23 @@
 package com.literandltx.taskmcapp.repository;
 
 import com.literandltx.taskmcapp.model.Comment;
+import com.literandltx.taskmcapp.model.User;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByTaskId(final Pageable pageable, final Long taskId);
+    @EntityGraph(attributePaths = {"user", "task"})
+    List<Comment> findAllByUserAndTaskId(
+            final Pageable pageable,
+            final User user,
+            final Long taskId
+    );
+
+    @EntityGraph(attributePaths = {"user, task"})
+    Optional<Comment> findById(final Long id);
 }
