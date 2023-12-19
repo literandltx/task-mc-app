761a44587d57005712dffeb07a299a6fca26410a
literandltx <romanskovronn@gmail.com>
2023-12-20 01:24:42 +0200

    app/add logging and refactor task service

--- src/main/java/com/literandltx/taskmcapp/service/app/impl/TaskServiceImpl.java
+++ src/main/java/com/literandltx/taskmcapp/service/app/impl/TaskServiceImpl.java
@@ -20,12 +20,15 @@ import jakarta.transaction.Transactional;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -41,16 +44,18 @@ public class TaskServiceImpl implements TaskService {
             final Long projectId,
             final User user
     ) {
-        final Project project = projectRepository.findById(projectId).orElseThrow(
-                () -> new EntityNotFoundException("Cannot find project with id: " + projectId));
-        if (!Objects.equals(project.getUser().getId(), user.getId())) {
-            throw new RuntimeException("User do not have project with id: " + projectId);
-        }
+        final Project project = getProjectAndCheckPermission(projectId, user);
 
-        Task model = taskMapper.toModel(requestDto);
+        final Task model = taskMapper.toModel(requestDto);
         model.setProject(project);
 
-        return taskMapper.toDto(taskRepository.save(model), new HashSet<>());
+        final TaskResponseDto dto = taskMapper.toDto(taskRepository.save(model), new HashSet<>());
+
+        log.info(String.format(
+                "User: %s, saved taskId: %s, in projectId: %s",
+                user.getUsername(), dto.getId(), projectId));
+
+        return dto;
     }
 
     @Override
@@ -59,18 +64,20 @@ public class TaskServiceImpl implements TaskService {
             final User user,
             final Pageable pageable
     ) {
-        final Project project = projectRepository.findById(projectId).orElseThrow(
-                () -> new EntityNotFoundException("Cannot find project with id: " + projectId));
-        if (!Objects.equals(project.getUser().getId(), user.getId())) {
-            throw new RuntimeException("User have not project with id: " + projectId);
-        }
+        getProjectAndCheckPermission(projectId, user);
 
-        return taskRepository.findAllByProjectId(pageable, projectId).stream()
+        final List<TaskResponseDto> list = taskRepository.findAllByProjectId(pageable, projectId).stream()
                 .map(task -> {
                     task.setLabels(getTaskLabels(task));
                     return taskMapper.toDto(task, getAttachedFiles(task));
                 })
                 .toList();
+
+        log.info(String.format(
+                "User: %s, found all tasks in projectId: %s",
+                user.getUsername(), projectId));
+
+        return list;
     }
 
     @Override
@@ -79,18 +86,19 @@ public class TaskServiceImpl implements TaskService {
             final Long projectId,
             final User user
     ) {
-        final Project project = projectRepository.findById(projectId).orElseThrow(
-                () -> new EntityNotFoundException("Cannot find project with id: " + projectId));
-
-        if (!Objects.equals(project.getUser().getId(), user.getId())) {
-            throw new RuntimeException("User does not have project with id: " + projectId);
-        }
+        getProjectAndCheckPermission(projectId, user);
 
         final Task task = taskRepository.findByIdAndProjectId(id, projectId)
                 .orElseThrow(() -> new EntityNotFoundException("Cannot find task with id: " + id));
 
         task.setLabels(getTaskLabels(task));
-        return taskMapper.toDto(task, getAttachedFiles(task));
+        final TaskResponseDto dto = taskMapper.toDto(task, getAttachedFiles(task));
+
+        log.info(String.format(
+                "User: %s, found taskId: %s, in projectId: %s",
+                user.getUsername(), id, projectId));
+
+        return dto;
     }
 
     @Override
@@ -100,21 +108,29 @@ public class TaskServiceImpl implements TaskService {
             final Long projectId,
             final User user
     ) {
-        final Project project = projectRepository.findById(projectId).orElseThrow(
-                () -> new EntityNotFoundException("Cannot find project with id: " + projectId));
-        if (!Objects.equals(project.getUser().getId(), user.getId())) {
-            throw new RuntimeException("User have not project with id: " + projectId);
-        }
+        final Project project = getProjectAndCheckPermission(projectId, user);
 
         if (!taskRepository.existsByIdAndProjectId(id, projectId)) {
-            throw new RuntimeException("Cannot update task with id: " + id);
+            log.info(String.format(
+                    "User: %s, cannot delete: %s, in projectId: %s, taskId do not exists.",
+                    user.getUsername(), id, projectId));
+            throw new RuntimeException(String.format(
+                    "User: %s, cannot delete: %s, in projectId: %s, taskId do not exists.",
+                    user.getUsername(), id, projectId));
         }
 
         final Task model = taskMapper.toModel(requestDto);
         model.setId(id);
         model.setProject(project);
 
-        return taskMapper.toDto(taskRepository.save(model), getAttachedFiles(model));
+        final Task save = taskRepository.save(model);
+        final TaskResponseDto dto = taskMapper.toDto(save, getAttachedFiles(save));
+
+        log.info(String.format(
+                "User: %s, updated taskId: %s, in projectId: %s",
+                user.getUsername(), id, projectId));
+
+        return dto;
     }
 
     @Override
@@ -123,17 +139,22 @@ public class TaskServiceImpl implements TaskService {
             final Long projectId,
             final User user
     ) {
-        final Project project = projectRepository.findById(projectId).orElseThrow(
-                () -> new EntityNotFoundException("Cannot find project with id: " + projectId));
-        if (!Objects.equals(project.getUser().getId(), user.getId())) {
-            throw new PermissionDeniedException("User have no access to projectId: " + projectId);
-        }
+        getProjectAndCheckPermission(projectId, user);
 
         if (!taskRepository.existsByIdAndProjectId(id, projectId)) {
-            throw new RuntimeException("Cannot delete task with id: " + id);
+            log.info(String.format(
+                    "User: %s, cannot delete: %s, in projectId: %s, taskId do not exists.",
+                    user.getUsername(), id, projectId));
+            throw new RuntimeException(String.format(
+                    "User: %s, cannot delete: %s, in projectId: %s, taskId do not exists.",
+                    user.getUsername(), id, projectId));
         }
 
         taskRepository.deleteById(id);
+
+        log.info(String.format(
+                "User: %s, deleted taskId: %s, from projectId: %s",
+                user.getUsername(), id, projectId));
     }
 
     @Transactional
@@ -144,20 +165,30 @@ public class TaskServiceImpl implements TaskService {
             final Long projectId,
             final User user
     ) {
-        final Project project = projectRepository.findById(projectId).orElseThrow(
-                () -> new EntityNotFoundException("Cannot find project with id: " + projectId));
-        if (!Objects.equals(project.getUser().getId(), user.getId())) {
-            throw new PermissionDeniedException("User have no access to projectId: " + projectId);
-        }
+        getProjectAndCheckPermission(projectId, user);
 
         if (!taskRepository.existsByIdAndProjectId(taskId, projectId)) {
-            throw new RuntimeException("Cannot find task with id: " + taskId);
+            log.info(String.format(
+                    "User: %s, do not have taskId: %s, in projectId: %s",
+                    user.getUsername(), taskId, projectId));
+            throw new RuntimeException(String.format(
+                    "User: %s, do not have taskId: %s, in projectId: %s",
+                    user.getUsername(), taskId, projectId));
         }
         if (!labelRepository.existsByIdAndProjectId(labelId, projectId)) {
-            throw new RuntimeException("Cannot find label with id: " + labelId);
+            log.info(String.format(
+                    "User: %s, do not have labelId: %s, in projectId: %s",
+                    user.getUsername(), labelId, projectId));
+            throw new RuntimeException(String.format(
+                    "User: %s, do not have labelId: %s, in projectId: %s",
+                    user.getUsername(), labelId, projectId));
         }
 
         taskRepository.assignLabelToTask(taskId, labelId);
+
+        log.info(String.format(
+                "User: %s, assigned labelId: %s, to taskId: %s",
+                user.getUsername(), labelId, taskId));
     }
 
     @Override
@@ -167,13 +198,13 @@ public class TaskServiceImpl implements TaskService {
             final Long projectId,
             final User user
     ) {
-        final Project project = projectRepository.findById(projectId).orElseThrow(
-                () -> new EntityNotFoundException("Cannot find project with id: " + projectId));
-        if (!Objects.equals(project.getUser().getId(), user.getId())) {
-            throw new PermissionDeniedException("User have no access to projectId: " + projectId);
-        }
+        getProjectAndCheckPermission(projectId, user);
 
         taskRepository.removeLabelFromTask(taskId, labelId);
+
+        log.info(String.format(
+                "User: %s, removed labelId: %s, from taskId: %s",
+                user.getUsername(), labelId, taskId));
     }
 
     private Set<Label> getTaskLabels(
@@ -198,5 +229,32 @@ public class TaskServiceImpl implements TaskService {
         return attachmentRepository.findAllByTaskId(task.getId()).stream()
                 .map(Attachment::getFilename)
                 .collect(Collectors.toSet());
+    }
+
+    private Project getProjectAndCheckPermission(
+            final Long projectId,
+            final User user
+    ) {
+        final Optional<Project> project = projectRepository.findById(projectId);
+
+        if (project.isEmpty()) {
+            log.info(String.format(
+                    "User: %s, have not projectId: %s.",
+                    user.getUsername(), projectId));
+            throw new EntityNotFoundException(String.format(
+                    "User: %s, have not projectId: %s.",
+                    user.getUsername(), projectId));
+        }
+
+        if (!Objects.equals(project.get().getUser().getId(), user.getId())) {
+            log.info(String.format(
+                    "User: %s, have no access to projectId: %s",
+                    user.getUsername(), projectId));
+            throw new PermissionDeniedException(String.format(
+                    "User: %s, have no access to projectId: %s",
+                    user.getUsername(), projectId));
+        }
+
+        return project.get();
     }
 }
